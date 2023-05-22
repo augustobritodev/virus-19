336e788bbdb41ee650c440495ee9fb874549e0e8
Augusto Brito <augustobritodev@icloud.com>
2023-05-22 17:14:53 -0300

    fix: card and filter responsiveness

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -2,7 +2,7 @@
   <div class="mx-10 md:mx-120 lg:mx-60 my-5 bg-white border-2 rounded-md border-secondary">
     <div class="flex flex-col p-5 text-center ">
       <a href="#" class="text-center font-bold text-xl hover:underline decoration-solid">Brasil</a>
-      <dl class="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto sm:grid-cols-3 xl:grid-cols-6 sm:p-8">
+      <dl class="grid grid-cols-1 grid-rows-3 py-4 md:grid-cols-3 md:grid-rows-1">
         <div class="flex flex-col">
           <dd>Total de casos</dd>
           <dt class="text-primary mb-2 text-3xl font-bold">32.698.232</dt>
--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -6,7 +6,7 @@ import IconSearch from '@/components/icons/IconSearch.vue'
   <div class="mx-10 md:mx-120 lg:mx-60 bg-white border-2 rounded-md border-secondary p-10">
     <p class="text-center font-bold text-xl">Filtrar dados sobre um país</p>
     <div class="sm:flex items-center bg-white rounded overflow-hidden p-4 justify-between">
-      <div class="relative inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+      <div class="invisible sm:visible relative inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <IconSearch />
       </div>
       <input class="text-base  flex-grow outline-none px-4 " type="text" placeholder="Digite o nome do país" />
